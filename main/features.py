1fe42a8644d00b8b2191eb121f9f4fb7d4db35c2
Matías Di Bernardo <matiasdiber7@gmail.com>
2022-11-20 13:01:38 -0300

    Define feature window

--- main/features.py
+++ main/features.py
@@ -1,6 +1,7 @@
 import librosa
 import numpy as np
 import scipy
+import random
 from keras import Sequential
 
 #Extract x-vector of audio, ver si guardar toda la data en memoria o usar un generator
@@ -8,9 +9,15 @@ from keras import Sequential
 class FeatureExtraction():
     '''
     Extract the relevant frecuency information from the audio
+    win_time: Interval of time to analize audio
+    mel_coeff: Amount of mel coefficient to take into account
+    filter: If we want or not pre-processing to the audio
     '''
-    def __init__(self):
-        pass
+    def __init__(self, win_time, mel_coeff, filter):
+
+        self.win = win_time
+        self.mel_coeff = mel_coeff
+        self.filter = filter
 
     def check_length_and_pad(self, audio, fs):
         """
@@ -19,12 +26,26 @@ class FeatureExtraction():
         """
 
         if len(audio)/fs < self.win:
-            diff = self.win * fs - len(audio)//fs
+            diff = int(self.win * fs) - len(audio)//fs
 
-            return np.hstack([np.zeros(diff), audio])  #Hacer que sea adelante o atras random
+            random_diff = random.randint(0, diff - 1)
+
+            return np.hstack([np.zeros(diff - random_diff), audio, np.zeros(random_diff)])
         
         else:
             return audio[:fs*self.win]
+
+    def filter_signal(self, audio, fs):
+        """
+        Apply filter to the signal to analize only the relevant
+        frecuencies for speakers
+        """
+
+        #Hacer pasa alto y demás 
+
+        pass
+
+
 
     def get_mel(self):
         pass
